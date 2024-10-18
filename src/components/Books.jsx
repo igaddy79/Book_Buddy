4b90974031fd55c4f745372c0dcb369e306f43b5
iamwm999 <166542644+iamwm999@users.noreply.github.com>
2024-10-18 14:44:36 -0700

    Update Books.jsx
    show available

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -3,45 +3,54 @@ import { Link } from "react-router-dom";
 import { fetchAllBooks } from "../api/index";
 import './Books.css';
 
+
 export default function Books() {
-  const [books, setBooks] = useState([]);
+ const [books, setBooks] = useState([]);
 
-  useEffect(() => {
-    async function getAllBooks() {
-      try {
-        const booksData = await fetchAllBooks();
-        setBooks(booksData || []);
-      } catch (error) {
-        console.error("Could not fetch all books:", error);
-      }
-    }
 
-    getAllBooks();
-  }, []);
+ useEffect(() => {
+   async function getAllBooks() {
+     try {
+       const booksData = await fetchAllBooks();
+       setBooks(booksData || []);
+     } catch (error) {
+       console.error("Could not fetch all books:", error);
+     }
+   }
 
-  return (
-    <>
-      {books.length > 0 ? (
-        <div className="books-container">
-          <h2 className="header">Available Books</h2>
-          <br />
-          {books.map((book) => (
-            <div className="book-box" key={book.id}>
-              <h4>{book.title}</h4>
-              {book.coverimage && (
-                <img
-                  src={book.coverimage} 
-                  alt={`Cover of ${book.title}`}
-                  className="book-image"
-                />
-              )}
-              <Link to={`/Books/${book.id}`}>View Book Details</Link>
-            </div>
-          ))}
-        </div>
-      ) : (
-        <p>Loading All Books...</p>
-      )}
-    </>
-  );
+
+   getAllBooks();
+ }, []);
+
+
+
+
+
+
+ return (
+   <>
+     {books.length > 0 ? (
+       <div className="books-container">
+         <h2 className="header">ALL BOOKS</h2>
+         <br />
+         {books.map((book) => (
+           <div className="book-box" key={book.id}>
+             <h4>{book.title}</h4>
+             {book.coverimage && (
+               <img
+                 src={book.coverimage}
+                 alt={`Cover of ${book.title}`}
+                 className="book-image"
+               />
+             )}
+             <Link to={`/Books/${book.id}`}>View Book Details</Link>
+           </div>
+         ))}
+       </div>
+     ) : (
+       <p>Loading All Books...</p>
+     )}
+   </>
+ );
 }
+
